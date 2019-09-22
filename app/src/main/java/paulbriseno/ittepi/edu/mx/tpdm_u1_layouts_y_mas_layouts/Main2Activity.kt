b44cfb81b95e856437012c7e16ed3a57993f0308
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2019-09-22 03:21:33 -0500

    Initial commit

---
+++ app/src/main/java/paulbriseno/ittepi/edu/mx/tpdm_u1_layouts_y_mas_layouts/Main2Activity.kt
@@ -0,0 +1,12 @@
+package paulbriseno.ittepi.edu.mx.tpdm_u1_layouts_y_mas_layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
